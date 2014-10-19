f490bfbcc67ce3d2208ce3fc34d0fb1575b56940
Aram Kocharyan <akarmenia@gmail.com>
2014-10-19 17:22:09 +1100

    Fixed clean task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,6 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('build', 'Build a distributable package.', ['coffee:dist']);
-  grunt.registerTask('clean', 'Cleans built files.', ['clean:dist']);
   grunt.registerTask('test', 'Runs tests.', ['mochaTest']);
   grunt.registerTask('inspector', 'Runs node-inspector.', ['node-inspector:dev']);
   grunt.registerTask('test:ci', 'Runs tests.', ['mochaTest']);
